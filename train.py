4339c88da75577d18bc2e03ab24d697a51d1c13a
jgpeoc <jgpeoc@gmail.com>
2022-10-20 18:14:13 -0700

    finish train

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 from dataset import OxfordPets
 from tensorflow import keras
+from keras import backend as K
 from unet import get_model
 import random
 import os
@@ -8,10 +9,10 @@ import mlflow
 import mlflow.keras
 
 # Add mlflow logging
+mlflow.set_tracking_uri('.')
 
-
-input_dir =
-target_dir =
+input_dir = './data/images/'
+target_dir = './data/annotations/'
 img_size = (160, 160)
 num_classes = 3
 batch_size = 32
@@ -25,7 +26,7 @@ target_img_paths = sorted([
 ])
 
 # Free up RAM in case the model definition cells were run multiple times
-
+K.clear_session()
 
 # Build model
 model = get_model(img_size, num_classes)
@@ -46,10 +47,15 @@ val_gen = OxfordPets(batch_size, img_size, val_input_img_paths, val_target_img_p
 model.compile(optimizer="rmsprop", loss="sparse_categorical_crossentropy")
 
 # Set the keras callback using ModelCheckpoint to save the best results of the model
-callbacks =
+callbacks = keras.callbacks.ModelCheckpoint(
+    filepath='./checkpoint',
+    save_weights_only=True,
+    monitor='val_accuracy',
+    mode='max',
+    save_best_only=True)
 
 # Train the model, doing validation at the end of each epoch.
 # Make sure to include the calllback for mlflow
 epochs = 15
-model.fit(train_gen, epochs=epochs, validation_data=val_gen)
+model.fit(train_gen, epochs=epochs, validation_data=val_gen, callbacks=[callbacks])
 model.save('./segmentation/')
